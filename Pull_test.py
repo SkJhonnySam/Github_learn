137764acceeba64f3acce0adedf8e39a3e279c85
SkJhonnySam <srsam1819@gmail.com>
2025-03-01 15:24:30 +0530

    Pull_test.py file moified

--- Pull_test.py
+++ Pull_test.py
@@ -1,3 +1,3 @@
-Print("If i want get recently added file in Hithub")
+print("If i want get recently added file in Hithub")
 print(" Use the 'pull' cmd to bring ")
 print(" 'git pull' will bring all changes whether it added or modify, or deleted")
